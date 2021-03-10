e5f896e35998cb4df0282a03e47ec43743352c80
imali <chjczj@126.com>
2021-03-10 17:29:18 +0800

    vuepress init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "JavaScript-The-Definitive-Guide-7th-zh",
+	description: "JavaScript-The-Definitive-Guide-7th-zh",
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/nprogress',
+    '@vuepress/pwa',
+    '@vuepress/search',
+  ],
+};
